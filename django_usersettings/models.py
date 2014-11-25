1e6eb0924ef9fed5dc0da51df782ae091e4f47ef
Dryice Liu <dryiceliu@gmail.com>
2014-11-26 00:37:36 +0800

    get and set arbitrary setting

--- django_usersettings/models.py
+++ django_usersettings/models.py
@@ -11,6 +11,35 @@ class UserSetting(models.Model):
     def __str__(self):
         return self.user.username
 
+    def __getattr__(self, k):
+        try:
+            return object.__getattribute__(self, k)
+        except AttributeError:
+            try:
+                asObject = ArbitrarySetting.objects.get(
+                    setting_id = self.id,
+                    key=k,
+                )
+                return asObject.value
+            except ArbitrarySetting.DoesNotExist:
+                raise AttributeError(k)
+
+    def __setattr__(self, k, v):
+        try:
+            object.__getattribute__(self, k)
+        except AttributeError:
+            if k.startswith("as_"):
+                asObject, created = ArbitrarySetting.objects.get_or_create(
+                    setting_id = self.id,
+                    key = k,
+                )
+                asObject.value = v
+                asObject.save()
+            else:
+                object.__setattr__(self, k, v)
+        else:
+            object.__setattr__(self, k, v)
+
 
 class ArbitrarySetting(models.Model):
     setting_id = models.IntegerField()
