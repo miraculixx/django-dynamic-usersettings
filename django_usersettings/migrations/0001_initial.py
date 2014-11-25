e22b93afddd1845e16dc062a8c99d935eb15ab05
Dryice Liu <dryiceliu@gmail.com>
2014-11-25 16:28:24 +0800

    initial DB setup and migration

---
+++ django_usersettings/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ArbitrarySetting',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(max_length=32)),
+                ('value', models.CharField(max_length=128)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserSetting',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(related_name='settings', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='arbitrarysetting',
+            name='setting',
+            field=models.ForeignKey(to='django_usersettings.UserSetting'),
+            preserve_default=True,
+        ),
+    ]
--- django_usersettings/models.py
+++ django_usersettings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserSetting(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="settings",
+    )
+
+
+class ArbitrarySetting(models.Model):
+    setting = models.ForeignKey(UserSetting)
+    key = models.CharField(max_length=32)
+    value = models.CharField(max_length=128)
