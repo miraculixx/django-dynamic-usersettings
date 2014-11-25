bc09787280f1d34cd3831da85745c3064208f90c
Dryice Liu <dryiceliu@gmail.com>
2014-11-25 16:45:01 +0800

    admin interface

--- django_usersettings/admin.py
+++ django_usersettings/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django_usersettings.models import UserSetting, ArbitrarySetting
 
-# Register your models here.
+
+class ArbitrarySettingInline(admin.TabularInline):
+    model = ArbitrarySetting
+    extra = 3
+    
+
+class ArbitrarySettingAdmin(admin.ModelAdmin):
+    list_display=('setting', 'key', 'value')
+    
+admin.site.register(ArbitrarySetting, ArbitrarySettingAdmin)
+
+
+class UserSettingAdmin(admin.ModelAdmin):
+    inlines = [ArbitrarySettingInline,]
+    
+admin.site.register(UserSetting, UserSettingAdmin)
+
+
