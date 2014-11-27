94c1bb4c2320c80f05af7f1a931832b4f88b4e93
Dryice Liu <dryiceliu@gmail.com>
2014-11-27 17:05:26 +0800

    API get
    GET settings/<user.pk> => return the JSON object containing all the settings

--- django_usersettings/api.py
+++ django_usersettings/api.py
@@ -1,10 +1,41 @@
+import json
+
 from tastypie.authorization import DjangoAuthorization
-from tastypie.resources import ModelResource
+from tastypie.resources import Resource
+from tastypie.bundle import Bundle
 from django_usersettings.models import UserSetting
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+from libs import SettingGateWay
 
 
-class UserSettingResource(ModelResource):
+class UserSettingResource(Resource):
     class Meta:
-        queryset = UserSetting.objects.all()
-        resource_name = 'arbitrary_setting'
+        resource_name = 'settings'
         authorization= DjangoAuthorization()
+
+    def detail_uri_kwargs(self, bundle_or_obj):
+        kwargs = {}
+
+        if isinstance(bundle_or_obj, Bundle):
+            kwargs['pk'] = bundle_or_obj.obj._user.pk
+        else:
+            kwargs['pk'] = bundle_or_obj._user.pk
+
+        return kwargs
+    
+    def dehydrate(self, bundle):
+        user = bundle.obj._user
+        for setting in UserSetting.objects.filter(user=user):
+            bundle.data[setting.field_name] = json.dumps({
+                'label': setting.label,
+                'type': setting.field_type,
+                'value': setting.value,
+                })
+ 
+        return bundle
+
+    def obj_get(self, bundle, **kwargs):
+        user = get_object_or_404(get_user_model(), pk=kwargs['pk'])
+        return SettingGateWay(user)
