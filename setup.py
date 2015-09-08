6bde775eab32c4ebd177232bc4061379e6bc2814
miraculixx <ps@novapp.ch>
2015-09-08 15:20:17 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_dynamic_usersettings',
-    version='0.1',
+    version='0.1.3rc',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django-tastypie'],
